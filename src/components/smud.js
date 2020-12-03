2ae106e2605ea02f0d91d7b5a8f7f1bf060a1c3f
Camilla Lund <camilla.lund@sparebank1.no>
2020-12-03 15:53:20 +0100

    Versjon 1.0

--- src/components/smud.js
+++ src/components/smud.js
@@ -6,7 +6,15 @@ import React from 'react'
 const Smud = () => (
   <div className="smud-logo">
     <div className="smud-logo__text smud-logo__start">[sm</div>
-    <div className="smud-logo__text smud-logo__u">u</div>
+    <div className="smud-logo__text">
+      <div className="smud-logo__text smud-logo__u">u</div>
+      <div className="smud-logo__text smud-logo__hale smud-logo__u2">u</div>
+      <div className="smud-logo__text smud-logo__hale smud-logo__u3">u</div>
+      <div className="smud-logo__text smud-logo__hale smud-logo__u4">u</div>
+      <div className="smud-logo__text smud-logo__hale smud-logo__u5">u</div>
+      <div className="smud-logo__text smud-logo__hale smud-logo__u6">u</div>
+      <div className="smud-logo__text smud-logo__hale smud-logo__u7">u</div>
+    </div>
     <div className="smud-logo__text smud-logo__end">:d]</div>
   </div>
 )
