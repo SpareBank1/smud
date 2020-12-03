5830a08b403806365e00d0898ab72b85e6d0b599
Camilla Lund <camilla.lund@sparebank1.no>
2020-12-03 16:05:50 +0100

    Fikset konflikt med klassenavn

--- src/components/smud.js
+++ src/components/smud.js
@@ -4,18 +4,18 @@ import React from 'react'
 
 
 const Smud = () => (
-  <div className="smud-logo">
-    <div className="smud-logo__text smud-logo__start">[sm</div>
-    <div className="smud-logo__text">
-      <div className="smud-logo__text smud-logo__u">u</div>
-      <div className="smud-logo__text smud-logo__hale smud-logo__u2">u</div>
-      <div className="smud-logo__text smud-logo__hale smud-logo__u3">u</div>
-      <div className="smud-logo__text smud-logo__hale smud-logo__u4">u</div>
-      <div className="smud-logo__text smud-logo__hale smud-logo__u5">u</div>
-      <div className="smud-logo__text smud-logo__hale smud-logo__u6">u</div>
-      <div className="smud-logo__text smud-logo__hale smud-logo__u7">u</div>
+  <div className="a-smud-logo">
+    <div className="a-smud-logo__text a-smud-logo__start">[sm</div>
+    <div className="a-smud-logo__text">
+      <div className="a-smud-logo__text a-smud-logo__u">u</div>
+      <div className="a-smud-logo__text a-smud-logo__hale a-smud-logo__u2">u</div>
+      <div className="a-smud-logo__text a-smud-logo__hale a-smud-logo__u3">u</div>
+      <div className="a-smud-logo__text a-smud-logo__hale a-smud-logo__u4">u</div>
+      <div className="a-smud-logo__text a-smud-logo__hale a-smud-logo__u5">u</div>
+      <div className="a-smud-logo__text a-smud-logo__hale a-smud-logo__u6">u</div>
+      <div className="a-smud-logo__text a-smud-logo__hale a-smud-logo__u7">u</div>
     </div>
-    <div className="smud-logo__text smud-logo__end">:d]</div>
+    <div className="a-smud-logo__text a-smud-logo__end">:d]</div>
   </div>
 )
 
